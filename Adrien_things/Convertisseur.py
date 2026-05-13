60111e2f1443ec972521c2998d6608fdc12e877a
dinolol15 <adrien.buschbeck@gmail.com>
2026-05-13 10:38:27 +0200

    new_file

---
+++ Adrien_things/Convertisseur.py
@@ -0,0 +1,31 @@
+"""
+Programme qui convertit une matrice de tile en une matrice de bytes pour l'affichage
+
+Auteur: Adrien Buschbeck
+"""
+
+from matrix_manager import Matrix, get
+from Tile import Tile
+
+
+def convertisseur(matrix: Matrix[Tile]) -> bytes:
+    """
+    Convertit une matrice de tile en une matrice de bytes pour l'affichage
+    """
+    coords = [
+        (i, j)
+        for j in range(len(matrix[len(matrix) - 1]))
+        for i in range(len(matrix))
+    ]
+    bytestr: bytes = b""
+    for c in coords:
+        print(c)
+        cell = get(matrix, c)
+        cell_new = cell.Color + (255,)
+        print(cell_new)
+        bb = b""
+        for i in cell_new:
+            bb += i.to_bytes(1)
+        bytestr += bb
+
+    return bytestr
